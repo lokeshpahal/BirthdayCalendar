5fd0648a964752aa324acc3cb9b41f4664d9c800
unknown <tomerpacific@gmail.com>
2021-12-13 23:35:44 +0200

    BirthdayCalendar adding logic for UpdateNotificationStatus event

--- lib/BLoC/birthday_bloc.dart
+++ lib/BLoC/birthday_bloc.dart
@@ -2,23 +2,57 @@ import 'dart:async';
 
 import 'package:birthday_calendar/BLoC/bloc.dart';
 import 'package:birthday_calendar/model/user_birthday.dart';
+import 'package:birthday_calendar/service/notification_service.dart';
+import 'package:birthday_calendar/service/shared_prefs.dart';
+
+import 'BirthdayEvent.dart';
 
 class BirthdayBloc implements Bloc {
-  UserBirthday? _userBirthday;
+  late UserBirthday _userBirthday;
 
   final _birthdayController = StreamController<UserBirthday>();
 
+
+  StreamSink<UserBirthday> get _inBirthday => _birthdayController.sink;
   Stream<UserBirthday> get birthdayStream => _birthdayController.stream;
 
-  void addBirthday(UserBirthday birthday) {
+  final _birthdayEventController = StreamController<BirthdayEvent>();
+
+  Sink<BirthdayEvent> get birthdayEventSink => _birthdayEventController.sink;
+
+  BirthdayBloc(UserBirthday birthday) {
     _userBirthday = birthday;
-    _birthdayController.sink.add(birthday);
+    _birthdayEventController.stream.listen(_mapEventToState);
   }
 
+  void _mapEventToState(BirthdayEvent event) {
+    if (event is GetBackgroundColor) {
+
+    } else if (event is UpdateNotificationStatus) {
+      _handleNotificationStatusForUser();
+    }
+
+    _inBirthday.add(_userBirthday)
+  }
+
+  void _handleNotificationStatusForUser() {
+    _userBirthday.hasNotification = !_userBirthday.hasNotification;
+    SharedPrefs().updateNotificationStatusForBirthday(
+        _userBirthday, _userBirthday.hasNotification);
+    if (_userBirthday.hasNotification) {
+      NotificationService()
+          .cancelNotificationForBirthday(_userBirthday);
+    } else {
+      NotificationService().scheduleNotificationForBirthday(
+          _userBirthday,
+          "${_userBirthday.name} has an upcoming birthday!");
+    }
+  }
 
   @override
   void dispose() {
     _birthdayController.close();
+    _birthdayEventController.close();
   }
 
 }
